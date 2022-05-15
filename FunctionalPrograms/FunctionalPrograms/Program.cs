4111bdb513d5b18d7f4dd06852ef12a7fcfce94e
prashantmagare <prashantmagare3010@gmail.com>
2022-05-15 17:56:22 +0530

    Welcome to Functional Programs

---
+++ FunctionalPrograms/FunctionalPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day5BasicPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To Basic Core Programs");
+        }
+    }
+}
