b070978b7b48baae4b2c68de952b852c6520e622
prashantmagare <prashantmagare3010@gmail.com>
2022-05-15 18:43:01 +0530

    Added Power of 2 Program

--- FunctionalPrograms/FunctionalPrograms/Program.cs
+++ FunctionalPrograms/FunctionalPrograms/Program.cs
@@ -3,10 +3,31 @@
 namespace Day5BasicPrograms
 {
     internal class Program
-    {
-        static void Main(string[] args)
+    { 
+         public class Power_Of_2
         {
-            Console.WriteLine("Welcom To Basic Core Programs");
+            public void Power_Of_Two(int number)
+            {
+                int temp = number;
+                int result = 1;
+                while (number != 0)
+                {
+                    result *= 2;
+                    number--;
+                }
+                Console.WriteLine($"Power of 2 ^ {temp} is : {result}");
+          
+            }
+        
+            public static void Main(string[] args)
+            {
+                Power_Of_2 p2 = new Power_Of_2(); //Create Object
+
+                Console.WriteLine("Enter the number ");  //take inpute from user
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                p2.Power_Of_Two(number); //Call Methode\
+            }
         }
     }
 }
