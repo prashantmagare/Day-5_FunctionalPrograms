9e9b4ab19b48f32cb60f4a517205ac86a8a26a4a
prashantmagare <prashantmagare3010@gmail.com>
2022-05-15 18:32:38 +0530

    Added Leap Year Program

--- FunctionalPrograms/FunctionalPrograms/Program.cs
+++ FunctionalPrograms/FunctionalPrograms/Program.cs
@@ -4,9 +4,25 @@ namespace Day5BasicPrograms
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class LeapYear
         {
-            Console.WriteLine("Welcom To Basic Core Programs");
+            public int year;
+
+        }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the Year");
+                int year = Convert.ToInt32(Console.ReadLine());
+                LeapYear ly = new LeapYear();
+
+                if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+                {
+                    Console.WriteLine($"given year {year} is Leap Year");
+                }
+                else
+                {
+                    Console.WriteLine($"given year {year} is not a Leap Year");
+                }
+            }
         }
     }
-}
