d6c35101d1a55cc3ef3cef6303e6f241ff74bdec
prashantmagare <prashantmagare3010@gmail.com>
2022-05-15 18:19:11 +0530

    Added Flip Coin Program

--- FunctionalPrograms/FunctionalPrograms/Program.cs
+++ FunctionalPrograms/FunctionalPrograms/Program.cs
@@ -4,9 +4,41 @@ namespace Day5BasicPrograms
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class FlipCoinprogram
         {
-            Console.WriteLine("Welcom To Basic Core Programs");
+            int headcount = 0;
+            int tailcount = 0;
+            public void FlipCoinMethod(int input)
+            {
+                int temp = input;
+                Random r = new Random();
+                while (input != 0)
+                {
+
+                    double random = r.NextDouble();
+                    if (random <= 0.5)
+                    {
+                        tailcount++;
+                    }
+                    else
+                    {
+                        headcount++;
+                    }
+                    input--;
+
+                }
+                Console.WriteLine((headcount * 101) / temp);
+                Console.WriteLine((tailcount * 101) / temp);
+
+            }
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the number : ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                FlipCoinprogram f = new FlipCoinprogram();
+                f.FlipCoinMethod(number);
+            }
+
         }
     }
 }
